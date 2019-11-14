8754b565602b6a4f6c1a202bb5056bbb226fe418
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2019-11-14 15:36:21 +0200

    setup login reducer

--- src/redux/actions/loginActions.js
+++ src/redux/actions/loginActions.js
@@ -1,10 +1,13 @@
 import {
-    CHANGE_AUTH_LAYOUT, FORGET_PASSWORD_INPUT_UPDATE,
+    CHANGE_AUTH_LAYOUT,
     LOGIN_INPUT_UPDATE,
-    REGISTER_INPUT_UPDATE, SUBMIT_FORGET_PASSWORD_EMAIL,
+    REGISTER_INPUT_UPDATE,
     SUBMIT_LOGIN_EMAIL,
     SUBMIT_LOGIN_PASSWORD,
-    SUBMIT_REGISTER_FORM
+    SUBMIT_REGISTER_FORM,
+    FORGET_PASSWORD_INPUT_UPDATE,
+    SUBMIT_FORGET_PASSWORD_EMAIL,
+
 } from "../types";
 
 export const login_input_update = () => {
@@ -18,16 +21,19 @@ const submit_login_email_success = () => {
         type: SUBMIT_LOGIN_EMAIL,
     };
 };
+
 export const submit_login_email = () => {
     return {
         type: SUBMIT_LOGIN_EMAIL,
     };
 };
+
 const submit_login_password_success = () => {
     return {
         type: SUBMIT_LOGIN_PASSWORD,
     };
 };
+
 export const submit_login_password = () => {
     return {
         type: SUBMIT_LOGIN_PASSWORD,
@@ -39,33 +45,39 @@ export const register_input_update = () => {
         type: REGISTER_INPUT_UPDATE,
     };
 };
+
 const submit_register_success = () => {
     return {
         type: SUBMIT_REGISTER_FORM,
     };
 };
+
 export const submit_register_form = () => {
     return {
         type: SUBMIT_REGISTER_FORM,
     };
 };
+
 export const change_auth_layout = (layout = 'login') => {
     return {
         type: CHANGE_AUTH_LAYOUT,
         payload: { layout }
     };
 };
+
 export const forget_password_email_update = value => {
     return {
         type: FORGET_PASSWORD_INPUT_UPDATE,
         payload: { value }
     };
 };
+
 const forget_password_email_submit_success = () => {
     return {
         type: SUBMIT_FORGET_PASSWORD_EMAIL
     };
 };
+
 export const forget_password_email_submit = () => {
     return {
         type: SUBMIT_FORGET_PASSWORD_EMAIL
--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -1,3 +1,15 @@
+import {
+    CHANGE_AUTH_LAYOUT,
+    LOGIN_INPUT_UPDATE,
+    REGISTER_INPUT_UPDATE,
+    SUBMIT_LOGIN_EMAIL,
+    SUBMIT_LOGIN_PASSWORD,
+    SUBMIT_REGISTER_FORM,
+    FORGET_PASSWORD_INPUT_UPDATE,
+    SUBMIT_FORGET_PASSWORD_EMAIL,
+
+} from "../types";
+
 const INITIAL_STATE = {
     layout: 'login',
     login: {
@@ -16,6 +28,22 @@ const INITIAL_STATE = {
 };
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case CHANGE_AUTH_LAYOUT:
+            return state;
+        case LOGIN_INPUT_UPDATE:
+            return state;
+        case REGISTER_INPUT_UPDATE:
+            return state;
+        case SUBMIT_LOGIN_EMAIL:
+            return state;
+        case SUBMIT_LOGIN_PASSWORD:
+            return state;
+        case SUBMIT_REGISTER_FORM:
+            return state;
+        case FORGET_PASSWORD_INPUT_UPDATE:
+            return state;
+        case SUBMIT_FORGET_PASSWORD_EMAIL:
+            return state;
         default:
             return state;
     }
